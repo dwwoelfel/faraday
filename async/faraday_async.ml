dabeaea9035101cacc489130d6cc314c7b0c323b
Daniel Woelfel <dwwoelfel@gmail.com>
2019-10-10 15:54:24 -0700

    Update faraday_async.ml

--- async/faraday_async.ml
+++ async/faraday_async.ml
@@ -22,7 +22,7 @@ let serialize t ~yield ~writev =
       yield t >>= fun () -> loop t
     | `Close -> return ()
   in
-  (try_with ~extract_exn:true (fun () -> loop t))
+  (Async_kernel.try_with ~extract_exn:true (fun () -> loop t))
   >>| function
     | Result.Ok () -> ()
     | Result.Error exn ->
